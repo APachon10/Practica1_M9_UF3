8f0095a0ce1ced6f893590274720b6e8494f3a37
Alberto Pachon <Pachondestroy95@gmail.com>
2019-05-15 13:35:48 +0200

    Clases De ejemplo Importadas·

---
+++ src/TCP/ServidorTCP.java
@@ -0,0 +1,67 @@
+package TCP;
+
+import java.io.*;
+import java.net.*;
+class ServidorTCP{
+	public static void main(String args[]){
+		// Primero indicamos la dirección IP local
+		InetAddress direcc =null;
+		try {
+			direcc = InetAddress.getLocalHost();
+		} catch (UnknownHostException e1) {
+			e1.printStackTrace();
+		}
+		try{
+			System.out.println("LocalHost = " +direcc.toString());
+		}
+		catch (Exception uhe){
+			System.err.println("No puedo saber la dirección IPlocal : " + uhe);
+		}
+		// Abrimos un "Socket de Servidor" TCP en el puerto 1234.
+		ServerSocket ss = null;
+		try{
+			ss = new ServerSocket(1234,0,InetAddress.getByName(null));
+			System.out.println("Socket: "+ss);
+		}
+		catch (IOException ioe){
+			System.err.println("Error al abrir el socket de servidor : " + ioe);
+			System.exit(-1);
+		}
+		int entrada;
+		long salida;
+		// Bucle infinito
+		while(true){
+			try{
+				// Esperamos a que alguien se conecte a nuestro
+				Socket sckt = ss.accept();
+				System.out.println("Socket 1 : " +sckt);
+				// Extraemos los Streams de entrada y de salida
+				DataInputStream dis = new DataInputStream(sckt.getInputStream());
+				System.out.println("Dis : "+dis);
+				DataOutputStream dos = new DataOutputStream(sckt.getOutputStream());
+				System.out.println("Dos : "+dos);
+				// Podemos extraer información del socket
+				// Nº de puerto remoto
+				int puerto = sckt.getPort();
+				System.out.println("Puerto: "+puerto);
+				// Dirección de Internet remota
+				InetAddress direcc2 = sckt.getInetAddress();
+				// Leemos datos de la peticion
+				entrada = dis.readInt();
+				// Calculamos resultado
+				salida = (long)entrada*(long)entrada;
+				// Escribimos el resultado
+				dos.writeLong(salida);
+				// Cerramos los streams
+				dis.close();
+				dos.close(); 
+				sckt.close();
+				// Registramos en salida estandard
+				System.out.println( "Cliente = " + direcc + ":"+ puerto+ "\tEntrada = " + entrada + "\tSalida = " +salida );
+			}
+			catch(Exception e){
+				System.err.println("Se ha producido laexcepción : " +e);
+			}
+		}
+	}
+} 
---
+++ src/TCP/clienteTCP.java
@@ -0,0 +1,58 @@
+package TCP;
+
+import java.io.*;
+import java.net.*;
+class ClienteTCP{
+	public static void main(String args[]){
+		// Leemos el primer parámetro, donde debe ir la dirección
+		// IP del servidor
+		InetAddress direcc = null;
+		try{
+			direcc = InetAddress.getByName(args[0]);
+			System.out.println("Direccion : "+ direcc);
+		}catch(UnknownHostException uhe){
+			System.err.println("Host no encontrado : " + uhe);
+			System.exit(-1);
+		}
+		// Puerto que hemos usado para el servidor
+		int puerto = 1234; 
+		// Para cada uno de los argumentos...
+		
+		for (int n=1;n<args.length;n++){
+			Socket sckt = null;
+			DataInputStream dis = null;
+			DataOutputStream dos = null;
+			try{
+				// Convertimos el texto en número
+				int numero = Integer.parseInt(args[n]);
+				// Creamos el Socket
+				sckt = new Socket(direcc,puerto);
+				System.out.println(sckt);
+				System.out.println("Socket cliente ; "+sckt);
+				// Extraemos los streams de entrada y salida
+				dis = new DataInputStream(sckt.getInputStream());
+				System.out.println("Dis : " +dis);
+				dos = new DataOutputStream(sckt.getOutputStream());
+				System.out.println("Dos : " +dos);
+				// Lo escribimos
+				dos.writeInt(numero);
+				// Leemos el resultado final
+				long resultado = dis.readLong();
+				// Indicamos en pantalla
+				System.out.println( "Solicitud = " + numero +" a\tResultado = " +resultado );
+				// y cerramos los streams y el socket
+				dis.close();
+				dos.close();
+			}
+			catch(Exception e){
+				System.err.println("Se ha producido la excepción : " +e);
+			}
+			try{
+				if (sckt!=null) sckt.close();
+			}
+			catch(IOException ioe){
+				System.err.println("Error al cerrar el socket :" + ioe);
+			}
+		}
+	}
+} 
